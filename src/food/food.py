194459aac04670e6c78a13d61b9a12db761c0644
Johnny Chen <chenjohn765@gmail.com>
2021-08-29 21:34:39 +0200

    created food list

---
+++ src/food/food.py
@@ -0,0 +1,69 @@
+from src.food.utils import Food
+
+
+class WhiteRice(Food):
+    name = "white_rice"
+
+
+class WhiteBread(Food):
+    name = "white_bread"
+
+
+class OatFlakes(Food):
+    name = "oat_flakes"
+
+
+class Pasta(Food):
+    name = "pasta"
+
+
+class ChickeBbreast(Food):
+    name = "chicken_breast"
+
+
+class BeefSteak(Food):
+    name = "beef_steak"
+
+
+class Salmon(Food):
+    name = "salmon"
+
+
+class Pork(Food):
+    name = "pork"
+
+
+class Ham(Food):
+    name = "ham"
+
+
+class Egg(Food):
+    name = "egg"
+
+
+class Whey(Food):
+    name = "whey"
+
+
+class Broccoli(Food):
+    name = "broccoli"
+
+
+class Courgette(Food):
+    name = "courgette"
+
+
+class Avocado(Food):
+    name = "avocado"
+
+
+class Almonds(Food):
+    name = "almonds"
+
+
+class Apple(Food):
+    name = "apple"
+
+
+class Banana(Food):
+    name = "banana"
